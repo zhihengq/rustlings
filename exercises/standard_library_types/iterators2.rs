2eeb171bd364de6fec5b52037e0f1ca6a56a2f8f
Zhiheng Qin <qinzhiheng24@gmail.com>
2020-09-20 05:08:23 -0700

    Finish `standard_library_types` section

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -7,14 +7,20 @@
 //         Try to ensure it returns a single string.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
-
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.collect::<String>() + c.as_str(),
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
+}
+
+pub fn capitalize_words<'a>(mut input: impl Iterator<Item = &'a &'a str>) -> Vec<String> {
+    let mut output = Vec::new();
+    while let Some(word) = input.next() {
+        output.push(capitalize_first(word));
+    }
+    output
 }
 
 #[cfg(test)]
@@ -37,14 +43,14 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let capitalized_words: Vec<String> = capitalize_words(words.iter());
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let capitalized_words = capitalize_words(words.iter()).join("");
         assert_eq!(capitalized_words, "Hello World");
     }
 }
